f6d5c4e90ea145da47858fde33c7307f9cf6aaf4
Rishabh Verma <rishabhverma8619@gmail.com>
2024-01-23 22:04:47 +0530

    Add files via upload

---
+++ Exercises/database_activation_table_creation.sql
@@ -0,0 +1,21 @@
+--  activate a database
+use b88;
+create table student (
+id int primary key not null,
+name text,
+marks int
+);
+select * from student;
+insert into student values
+(01,"Rahul", 45);
+insert into student values
+(02,"Rishabh", 50);
+insert into student values
+(03,"Param", 55);
+-- show me only id and marks
+select id, marks from student;
+-- create another table in the same database as studet_details
+create table student_details(
+id int primary key not null,
+address text,
+phone int);
